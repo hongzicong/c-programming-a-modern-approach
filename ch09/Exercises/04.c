e912a764399266b3edf075730e11457e9a5ca1d4
hongzicong <973138379@qq.com>
2017-02-25 14:20:52 +0800

    Update 04.c

--- ch09/Exercises/04.c
+++ ch09/Exercises/04.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int day_of_year(int, int, int);
+int day_of_year(int month,int day,int year);
+bool isLeap(int year);
 
-int main(void)
-{
-    printf("Day of year for date %.2d/%.2d/%.4d: %d\n", 4, 10, 2008,
-            day_of_year(4, 10, 2008)); //Leap year
-    printf("Day of year for date %.2d/%.2d/%.4d: %d\n", 4, 10, 2009,
-            day_of_year(4, 10, 2009)); //Non leap year
-    return 0;
+int main(void){
+	
+	printf("%d",day_of_year(4,10,2008));
+	return 0;
 }
 
-/* Returns the day in a year for a given
- * date */
-int day_of_year(int month, int day, int year)
-{
-    int i;
-    for (i = 1; i < month; i++) {
-        switch(i) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                day += 31;
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                day += 30;
-            case 2:
-                if (((year % 4 == 0) && (year % 100 != 0))
-                   || (year % 400 == 0))
-                    day += 29;
-                else
-                    day += 28;
-        }
-    }
+int day_of_year(int month,int day,int year){
+	
+	int i;
+	int sum=0;
+	int day_of_Feb;
+	
+	if(isLeap(year)){
+		day_of_Feb=30;
+	}else{
+		day_of_Feb=29;
+	}
+	
+	sum+=day;
+	
+	for(i=1;i<month;i++){
+		switch(i){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				sum+=31;
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				sum+=30;
+				break;
+			case 2:
+				sum+=day_of_Feb;
+				break;
+		}
+	}
+	return sum;
+}
 
-    return day;
+bool isLeap(int year){
+	return year%400==0||(year%100!=0&&year%4==0);
 }
