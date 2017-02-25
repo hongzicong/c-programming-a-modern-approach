5c71754c907dc828f7837c57ee6b5ad7587f9a44
hongzicong <973138379@qq.com>
2017-02-25 18:17:41 +0800

    Update 11.c

--- ch09/Exercises/11.c
+++ ch09/Exercises/11.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 
-#define SIZE sizeof(grades) / sizeof(grades[0])
+float compute_GPA(char grades[],int n);
 
-float compute_GPA(char grades[], int n);
-
-int main(void)
-{
-    char grades[10] = {'A', 'A', 'D', 'F', 'C', 'B', 'D', 'B', 'A', 'C'};
-
-    printf("Average of all grades: %.2f", compute_GPA(grades, SIZE));
-    return 0;
+int main(void){
+	
+	printf("%g",compute_GPA((char []){'A','B','C','D'},4));
+	return 0;
 }
 
-float compute_GPA(char grades[], int n)
-{
-    int i;
-    float average = 0.0f;
-    for (i = 0; i < n; i++) {
-        switch (grades[i]) {
-            case 'A':   average += 4.0f;
-                        break;
-            case 'B':   average += 3.0f;
-                        break;
-            case 'C':   average += 2.0f;
-                        break;
-            case 'D':   average += 1.0f;
-                        break;
-        }
-    }
-
-    return average / n;
+float compute_GPA(char grades[],int n){
+	
+	int i;
+	float GPA=0.0f;
+	
+	for(i=0;i<n;i++){
+		switch(grades[i]){
+			case 'A':
+				GPA+=4.0f;
+				break;
+			case 'B':
+				GPA+=3.0f;
+				break;
+			case 'C':
+				GPA+=2.0f;
+				break;
+			case 'D':
+				GPA+=1.0f;
+				break;
+			case 'F':
+				break;
+		}
+	}
+	
+	return GPA/n;
+	
 }
