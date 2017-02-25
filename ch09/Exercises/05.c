730daa531e334e2c46106d3440c38340a9f4face
hongzicong <973138379@qq.com>
2017-02-25 13:38:12 +0800

    Update 05.c

--- ch09/Exercises/05.c
+++ ch09/Exercises/05.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-int num_digits(int);
+int num_digits(int n);
 
-int main(void)
-{
-    printf("Number of digits in %d: %d", 1267123, num_digits(1267123)); 
-    return 0;
+int main(void){
+	printf("%d",num_digits(100));
+	return 0;
 }
 
-/* Returns the number of digits in n */
-int num_digits(int n)
-{
-    int digits = 0;
-
-    while (n > 0) {
-        n /= 10;
-        digits++;
-    }
-
-    return digits;
+int num_digits(int n){
+	int i=0;
+	
+	do{
+		n/=10;
+		i++;
+	}while(n!=0);
+	
+	return i;
 }
-
