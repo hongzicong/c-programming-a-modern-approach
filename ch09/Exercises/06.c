db4998cf8f68db4320a3c9a5e62dfc27b29ed07f
hongzicong <973138379@qq.com>
2017-02-25 13:32:07 +0800

    Update 06.c

--- ch09/Exercises/06.c
+++ ch09/Exercises/06.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-int digit(int, int);
+int digit(int n,int k);
 
-int main(void)
-{
-    printf("Digit %d (from right) in %d: %d", 4, 82934, digit(82934, 4));
-    return 0;
+int main(void){
+	printf("%d",num_digits(100));
+	return 0;
 }
 
-/* Return the kth digit in n (from the right) */
-int digit(int n, int k)
-{
-    int digit;
-
-    while (k > 0) {
-        digit = n % 10;
-        n /= 10;
-        k--;
-    }
-    return digit;
+int digit(int n,int k){
+	int i,result;
+	
+	for(i=0;i<k;i++){
+		result=n%10;
+		n/=10;
+	}
+	
+	return result;
 }
