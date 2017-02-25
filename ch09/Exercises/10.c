a8b62bcbc02c1b3ef7ed585aefa34da1f985c91e
hongzicong <973138379@qq.com>
2017-02-25 17:49:10 +0800

    Update 10.c

--- ch09/Exercises/10.c
+++ ch09/Exercises/10.c
@@ -1,47 +1,51 @@
 #include <stdio.h>
-#define SIZE sizeof(a) / sizeof(a[0])
 
-int largest_element(int a[], int length);
-float average_of_elements(int a[], int length);
-int positive_element_count(int a[], int length);
-int main(void)
-{
-    int i, a[10] = {1, 3, 8, 20, 12, 27, 0, 32, 12, 10};
-    printf("a[] contains: ");
-    for (i = 0; i < SIZE; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-    printf("Largest element in a[]: %d\n", largest_element(a, SIZE));
-    printf("Average of elements in a[]: %.2f\n", average_of_elements(a, SIZE));
-    printf("Number of positive elements in a[]: %d\n", positive_element_count(a, SIZE));
-    return 0;
+int max(int n,int a[n]);
+int posNumber(int n,int a[n]);
+int average(int n,int a[n]);
+
+int main(void){
+	
+	int a[10]={1,2,3,5};
+	
+	printf("%d %d %d",max(10,a),posNumber(10,a),average(10,a));
+	
+	return 0;
 }
-/* Returns the largest element in a[] */
-int largest_element(int a[], int length)
-{
-    int i, largest = a[0];
-    for (i = 1; i < length; i++) {
-        if (a[i] > largest)
-            largest = a[i];
-    }
-    return largest;
+
+int max(int n,int a[n]){
+	
+	int i,maxIndex=0;
+	
+	for(i=0;i<n;i++){
+		if(a[i]>a[maxIndex]){
+			maxIndex=i;
+		}
+	}
+	
+	return a[maxIndex];
 }
-/*Returns the average of all elements in a[] */
-float average_of_elements(int a[], int length)
-{
-    int i;
-    float average = a[0];
-    for (i = 1; i < length; i++)
-        average += a[i];
-    return average / length;
+
+int posNumber(int n,int a[n]){
+	
+	int i,posNum=0;
+	
+	for(i=0;i<n;i++){
+		if(a[i]>0)
+			posNum++;
+	}
+	
+	return posNum;
 }
-/*Returns the number of positive elements in a[] */
-int positive_element_count(int a[], int length)
-{
-    int i, positive_count = 0;
-    for (i = 0; i < length; i++) {
-        if (a[i] > 0)
-            positive_count++;
-    }
-    return positive_count;
+
+int average(int n,int a[n]){
+	
+	int i,sum=0;
+	
+	for(i=0;i<n;i++){
+		sum+=a[i];
+	}
+	
+	return sum/n;
+	
 }
