833a78d2c308a1a34bf2c0dbdeb27e864f22ca03
hongzicong <973138379@qq.com>
2017-02-26 13:02:41 +0800

    Update 07.c

--- ch10/Projects/07.c
+++ ch10/Projects/07.c
@@ -115,3 +115,114 @@ void print_digits_array(void)
         printf("\n");
     }
 }
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_DIGITS 10
+
+const int segments[10][7]= {{1,1,1,1,1,1},
+							{0,1,1},
+							{1,1,0,1,1,0,1},
+							{1,1,1,1,0,0,1},
+							{0,1,1,0,0,1,1},
+							{1,0,1,1,0,1,1},
+							{1,0,1,1,1,1,1},
+							{1,1,1},
+							{1,1,1,1,1,1,1},
+							{1,1,1,1,0,1,1}};
+
+char digits[3][MAX_DIGITS*4];
+
+void clear_digits_array(void);
+void process_digit(int digit,int position);
+void print_digits_array(void);
+
+int main(void){
+	
+	int i=0;
+	char ch;
+	
+	do{
+		clear_digits_array();
+		printf("\nEnter a number: ");
+		while((ch=getchar())!='\n'&&i<MAX_DIGITS){
+			if(ch>='0'&&ch<='9'){
+				process_digit(ch-'0',i);
+				i++;
+			}
+		}
+		print_digits_array();
+		printf("\nAgain? (Y/N)");
+		ch=getchar();
+	}while(ch=='Y',getchar());
+	
+	return 0;
+}
+
+void clear_digits_array(void){
+	
+	for(int i=0;i<3;i++){
+		for(int j=0;j<MAX_DIGITS*4;j++){
+			digits[i][j]=' ';
+		}
+	}
+	
+}
+	
+void process_digit(int digit,int position){
+	
+	int m=0;
+	int digitPosition[7];
+	
+	for(int i=0;i<7;i++){
+			digitPosition[i]=segments[digit][i];
+	}
+	
+	for(int i=0;i<3;i++){
+		for(int j=0;j<4;j++){
+			if(j==3||(j==0&&i==0)||(i==0&&j==2)){
+				digits[i][j+position*4]=' ';
+				continue;
+			}
+			else if(j==1){
+				if(i==0&&segments[digit][0]==1){
+					digits[i][j+position*4]='_';
+				}
+				if(i==1&&segments[digit][6]==1){
+					digits[i][j+position*4]='_';
+				}
+				if(i==2&&segments[digit][3]==1){
+					digits[i][j+position*4]='_';
+				}
+			}
+			else{
+				if(j==0&&i==1&&segments[digit][5]==1){
+					digits[i][j+position*4]='|';
+				}
+				if(j==0&&i==2&&segments[digit][4]==1){
+					digits[i][j+position*4]='|';
+				}
+				if(j==2&&i==1&&segments[digit][1]==1){
+					digits[i][j+position*4]='|';
+				}
+				if(j==2&&i==2&&segments[digit][2]==1){
+					digits[i][j+position*4]='|';
+				}
+			}
+		}
+	}
+	
+}
+
+void print_digits_array(void){
+	
+	for(int i=0;i<3;i++){
+		for(int j=0;j<MAX_DIGITS*4;j++){
+			putchar(digits[i][j]);
+		}
+		putchar('\n');
+	}
+	
+}
